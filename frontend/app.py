be91c303877ac72158afa2a79a6cf38833410269
jor2 <jordanwilliams997@hotmail.com>
2018-12-03 17:32:02 +0000

    updated to use kubelet's environment variables

--- frontend/app.py
+++ frontend/app.py
@@ -1,16 +1,19 @@
 from flask import Flask, jsonify
 import requests
 import json
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    url = "http://backend:5000/"
+    be_host = os.getenv('BACKEND_SERVICE_HOST', 'backend')
+    be_port = os.getenv('BACKEND_SERVICE_PORT', '5000')
+    url = 'http://{}:{}'.format(be_host, be_port)
     try:
         res = requests.get(url)
-    except ConnectionError:
+    except Exception:
         return "Error with {}".format(url)
     dictFromServer = res.json()
     return dictFromServer['message']
